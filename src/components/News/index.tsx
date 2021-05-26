bc69869ca1a24543459e4a0e29cb87ee4443e002
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-05-26 09:02:06 -0300

    List Elements

---
+++ src/components/News/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const News: React.FC = () => {
+  return (
+    <Container>
+      <span>Assuntos do momento no Brasil</span>
+      <strong>Se eu comprar 10kg de carne, ta pra 20 comer?</strong>
+    </Container>
+  );
+}
+
+export default News;
